d93de88ad78efc4d92c3606d99776b465c9c5b52
Dor Grinshpan <dorgrin6@gmail.com>
2017-11-09 11:10:03 +0200

    almost done

--- Polygons_1/Polygons.c
+++ Polygons_1/Polygons.c
@@ -1,63 +1,114 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <string.h>
 #include "Polygons.h"
 
 struct polygonsList polygons;
 void(*functionPointerArray[AMOUNT_OF_FUNCTIONS])(LLU);
 
+void parseInput(LLU input) {
+	LLU polygon = getPolygonFromInput(input);
 
-void main() {
-	BOOL continueInput = TRUE;
-	LLU input;
+	BOOL isInsert = input & 2; // 2nd bit
 
-	initFucntionPointerArray();
+	if (isInsert) {
+		add_polygon(polygon);
+	}
 
-	while (continueInput) {
-		scanf("%llx", &input);
-		printf("the number is: %llx\n", input);
-		
-		continueInput = createPolygonFromInput(input);
+	LLU whoToMakeOnMask = (input & 0x60) >> 6;
+
+	if (whoToMakeOnMask == CURRENT_POLYGON) {
+		do_current(input);
+	}
+	else {
+		do_all(input);
+	}
+	//area(input);
+}
+
+
+LLU getPolygonFromInput(LLU input) {
+	//get vertices
+	LLU polygon = input >> ACTION_BITS;
+
+	// get and add type
+	polygon <<= 1;
+
+	polygon += ((input & 4) >> 2);
+
+	return polygon;
+}
+
+ACTIONS_LIST getActionsList(LLU input) {
+	ACTIONS_LIST result;
+	//printf("input on getActionsList = %llx", input);
+	result.printAction = (input & 8) != 0;
+	result.perimeterAction = (input & 16) != 0;
+	result.areaAction = (input & 32) != 0;
+
+	return result;
+}
+
+void do_current(long long unsigned input)
+{
+	ACTIONS_LIST actionsList = getActionsList(input);
+
+	doActionsOnPolygon(polygons.tail, actionsList);
+}
+void do_all(long long unsigned input)
+{
+	ACTIONS_LIST actionsList = getActionsList(input);
+
+	LLU whoToMakeOnMask = (input & 0x60) >> 6;
+
+	LIST_NODE* currentPolygon = polygons.head;
+
+	while (currentPolygon != NULL) {
+		//doActionsOnPolygon(currentPolygon, isPrintAction, isPerimeterAction, isAreaAction);
+		if (isRightPolygon(currentPolygon->polygon, whoToMakeOnMask)) {
+			doActionsOnPolygon(currentPolygon, actionsList);
+		}
+
+		currentPolygon = currentPolygon->next;
+	}
+	
+}
+
+BOOL isRightPolygon(LLU polygon, LLU whoToMakeOnMask) {
+	BOOL result = FALSE;
+
+	if (whoToMakeOnMask == ALL_POLYGONS) {
+		result = TRUE;
+	}
+	else if (whoToMakeOnMask == ALL_SQUARES && isSquarePolygon(polygon)) {
+		result = TRUE;
+	}
+	else if (whoToMakeOnMask == ALL_TRIANGLES && isTrianglePolygon(polygon)) {
+		result = TRUE;
+	}
+
+	return result;
+}
+
+
+void doActionsOnPolygon(LIST_NODE* polygon, ACTIONS_LIST actionsList) {
+	if (actionsList.printAction) {
+		print_polygon(polygon->polygon);
+	}
+	if (actionsList.perimeterAction) {
+		perimeter(polygon->polygon);
+	}
+	if (actionsList.perimeterAction) {
+		area(polygon->polygon);
 	}
 }
 
-void initFucntionPointerArray() {
-	functionPointerArray[ADD_POLY_FUNC] = add_polygon;
-	functionPointerArray[PERIMETER_FUNC] = perimeter;
-	functionPointerArray[AREA_FUNC] = area;
-	functionPointerArray[PRINT_POLY_FUNC] = print_polygon;
-	functionPointerArray[DO_CURRENT_FUNC] = do_current;
-	functionPointerArray[DO_ALL_FUNC_FUNC] = do_all;
-}
-
-int getBitAt(LLU input, int bitIndex) {
-	int result = input & ((LLU)1 << bitIndex); // a mask for getting the k'th bit
-	result >>= bitIndex;
-	return result;
-}
-
-int getAndRemoveNextBit(LLU* input) {
-	int result = *input & 1;
-	*input <<= 1;
-	return result;
-}
-
-LIST_NODE* createListNode(LLU input) {
-	LIST_NODE *result = (LIST_NODE *)malloc(sizeof(LIST_NODE *));
+LIST_NODE* createListNode(LLU polygon) {
+	LIST_NODE *result = (LIST_NODE *)malloc(sizeof(LIST_NODE));
 	//TODO debug
-	printf("input: %llx\n", input);
-
-	//get vertices
-	LLU polygon = input >> 8;
-	printf("1.polygon: %llx\n", polygon);
-
-	// get and add type
-	polygon <<= 4;
-	printf("2.polygon: %llx\n", polygon);
-
-	polygon |= getBitAt(input, 2);
-	
-	printf("3.polygon: %llx\n", polygon);
+	printf("input: %llx\n", polygon);
 
 	result->polygon = polygon;
 	result->next = NULL;
@@ -78,39 +129,165 @@ void add_polygon(long long unsigned input) {
 	}
 }
 
-void perimeter(long long unsigned input)
-{
+float getEdgeLengthFromVertexes(VERTEX v1, VERTEX v2) {
+	int xDist = v1.x - v2.x;
+	int yDist = v1.y - v2.y;
+	return (float)sqrt(xDist * xDist + yDist * yDist); // sqrt((x1-x2)^2 + (y1-y2)^2)
 }
 
-void area(long long unsigned input)
-{
+float getEdgeLength(int vertexFirstIndex, LLU polygon) { 
+	VERTEX v1 = getVertexFromPolygon(vertexFirstIndex, polygon);
+	VERTEX v2 = getVertexFromPolygon(vertexFirstIndex + 12 ,polygon);
+
+	return getEdgeLengthFromVertexes(v1, v2);
 }
 
-void print_polygon(long long unsigned input)
-{
-}
+float getPerimeter(LLU polygon) {
+	int i;
+	float result = 0;
+	int numberOfVertices = getNumberOfVertices(polygon);
 
-void do_current(long long unsigned input)
-{
-}
-
-void do_all(long long unsigned input)
-{
-}
-
-
-
-BOOL createPolygonFromInput(LLU input)
-{
-	BOOL isLastCommand = (BOOL)getBitAt(input, 0);
-	int bitCounter = AMOUNT_OF_DIGITS_IN_INPUT - 1; // start bitcounter on 63rd bit
-
-	int nextBit = getBitAt(input, 1);
-
-	if (getBitAt(input, 1) != 0) {
-		functionPointerArray[ADD_POLY_FUNC](input);
+	VERTEX* vertices = (VERTEX*)malloc(numberOfVertices * sizeof(VERTEX));
+	
+	// get vertices
+	for (i = 0; i < numberOfVertices; i++) {
+		vertices[i] = getVertexFromPolygon(1 + i * 12, polygon);
 	}
 
-	return isLastCommand;
+	// get distance for all vertices and sum
+	// round the shape
+	for (i = 0; i < numberOfVertices - 1; i++) {
+		result += getEdgeLengthFromVertexes(vertices[i], vertices[i+1]);
+	}
+	// first and last
+	result += getEdgeLengthFromVertexes(vertices[0], vertices[numberOfVertices - 1]);
+
+	free(vertices);
+
+	return result;
 }
 
+void perimeter(long long unsigned polygon)
+{
+	printf("perimeter: %.1f\n", getPerimeter(polygon));
+}
+
+// using Heron's formula https://en.wikipedia.org/wiki/Heron's_formula
+float getTriangleArea(LLU triangle) {
+	int i;
+	VERTEX vertices[3];
+	for (i = 0; i < 3; i++) {
+		vertices[i] = getVertexFromPolygon(1 + 12 * i, triangle);
+	}
+
+	float a = getEdgeLengthFromVertexes(vertices[0], vertices[1]);
+	float b = getEdgeLengthFromVertexes(vertices[1], vertices[2]);
+	float c = getEdgeLengthFromVertexes(vertices[2], vertices[0]);
+	float s = (a + b + c) / 2;
+
+	return (float)sqrt(s*(s - a)*(s - b)*(s - c));
+}
+
+float getSquareArea(LLU square) {
+	return getTriangleArea(square) * 2;
+}
+
+void area(long long unsigned polygon)
+{
+	printf("area: ");
+
+	if (isSquarePolygon(polygon)) {
+		printf("%.1f", getSquareArea(polygon)); // TODO check
+	}
+	else if (isTrianglePolygon(polygon)) {
+		printf("%.1f", getTriangleArea(polygon));
+	}
+
+	printf("\n");
+}
+
+BOOL isSquarePolygon(LLU polygon) {
+	return (polygon & SQUARE) != 0;
+}
+
+BOOL isTrianglePolygon(LLU polygon) {
+	return !isSquarePolygon(polygon);
+}
+
+VERTEX getVertexFromPolygon(int vertexFirstBit, LLU polygon) {
+	VERTEX result;
+	LLU mask = 0x3F;					// we need 6 bits of the input i.e. 0011 1111
+									   // create the vertex value and move it to start
+	polygon >>= vertexFirstBit;
+	result.x = (char) mask & polygon;
+	result.x <<= 2;
+	result.x /= 4;
+	polygon >>= 6;
+	result.y = (char) mask & polygon;
+	result.y <<= 2;
+	result.y /= 4;
+
+	return result;
+}
+
+void printVertex(int vertexFirstBit, LLU polygon) {
+	VERTEX vertex = getVertexFromPolygon(vertexFirstBit, polygon);
+
+	printf("{%d, %d} ", vertex.x, vertex.y);
+}
+
+int getNumberOfVertices(LLU polygon) {
+	if (isSquarePolygon(polygon)) {
+		return 4;
+	}
+	else if (isTrianglePolygon(polygon)) {
+		return 3;
+	}
+	else {
+		return -1;
+	}
+}
+
+void print_polygon(long long unsigned polygon)
+{
+	int numberOfVertices = getNumberOfVertices(polygon);
+	int i;
+
+	isSquarePolygon(polygon) ? printf("Square ") : printf("Triangle ");
+
+	for (i = 0; i < numberOfVertices; i++) {
+		printVertex(1 + i * 12, polygon); // 
+	}
+
+	printf("\n");
+}
+
+int getBitAt(LLU input, int bitIndex) {
+	int result = input & ((LLU)1 << bitIndex); // a mask for getting the k'th bit
+	result >>= bitIndex;
+	return result;
+}
+
+void initFunctionPointerArray() {
+	functionPointerArray[ADD_POLY_FUNC] = add_polygon;
+	functionPointerArray[PERIMETER_FUNC] = perimeter;
+	functionPointerArray[AREA_FUNC] = area;
+	functionPointerArray[PRINT_POLY_FUNC] = print_polygon;
+	functionPointerArray[DO_CURRENT_FUNC] = do_current;
+	functionPointerArray[DO_ALL_FUNC_FUNC] = do_all;
+}
+
+void main() {
+	BOOL stopInput = FALSE;
+	LLU input;
+
+	initFunctionPointerArray();
+
+	while (!stopInput) {
+		scanf("%llx", &input);
+		//printf("the number is: %llx\n", input);
+
+		stopInput = (BOOL)getBitAt(input, 0);
+		parseInput(input);
+	}
+}
--- Polygons_1/Polygons.h
+++ Polygons_1/Polygons.h
@@ -1,8 +1,6 @@
 
 #ifndef _POLYGONS_H
 #define _POLYGONS_H
-
-
 #define FALSE 0
 #define TRUE 1
 #define AMOUNT_OF_DIGITS_IN_INPUT 64
@@ -10,7 +8,7 @@
 #define TRIANGLE 0
 #define SQUARE 1
 
-#define ACTION_BITS 7
+#define ACTION_BITS 8
 // functions
 #define AMOUNT_OF_FUNCTIONS 6
 #define ADD_POLY_FUNC 0
@@ -19,6 +17,14 @@
 #define PRINT_POLY_FUNC 3
 #define DO_CURRENT_FUNC 4
 #define DO_ALL_FUNC_FUNC 5
+
+// polygon bits
+#define CURRENT_POLYGON 0
+#define ALL_TRIANGLES 1
+#define ALL_SQUARES 2
+#define ALL_POLYGONS 3
+#define VERTEX_AMOUNT_OF_BITS 6
+#define TYPE_BIT 1
 
 typedef unsigned char BYTE;
 typedef unsigned char BOOL;
@@ -34,29 +40,48 @@ typedef struct polygonsList {
 	struct listNode* tail;
 }POLYGONS_LIST;
 
+typedef struct actionsList {
+	BOOL printAction;
+	BOOL perimeterAction;
+	BOOL areaAction;
+}ACTIONS_LIST;
 
-void initFucntionPointerArray();
-BOOL createPolygonFromInput(LLU input);
-int getAndRemoveNextBit(LLU* input);
-int getBitAt(LLU input, int bitIndex);
+typedef struct vertex {
+	char x;
+	char y;
+}VERTEX;
+
+
 
 void add_polygon(long long unsigned);
+float getEdgeLengthFromVertexes(VERTEX v1, VERTEX v2);
+float getPerimeter(LLU polygon);
 // add new polygon to the list
-
 void perimeter(long long unsigned);
-// calculate and print the perimeter
-
+float getTriangleArea(LLU triangle);
+float getSquareArea(LLU square);
+// calculate and print the perimete
 void area(long long unsigned);
+BOOL isSquarePolygon(LLU polygon);
+BOOL isTrianglePolygon(LLU polygon);
+VERTEX getVertexFromPolygon(int vertexFirstBit, LLU polygon);
+void printVertex(int vertexFirstBit, LLU polygon);
+int getNumberOfVertices(LLU polygon);
 // calculate and print the area
 void print_polygon(long long unsigned);
+int getBitAt(LLU input, int bitIndex);
+void initFunctionPointerArray();
+void parseInput(LLU input);
+LLU getPolygonFromInput(LLU input);
+ACTIONS_LIST getActionsList(LLU input);
 // print the type of polygon and its vertices
 void do_current(long long unsigned);
 // do the operations on the current polygon.
 void do_all(long long unsigned);
+BOOL isRightPolygon(LLU polygon, LLU whoToMakeOnMask);
+void doActionsOnPolygon(LIST_NODE * polygon, ACTIONS_LIST actionsList);
+LIST_NODE * createListNode(LLU input);
 // do the operations in the parameter on the list
 
 
-
 #endif // !_POLYGONS_H
-
-
